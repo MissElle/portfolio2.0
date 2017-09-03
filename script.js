eb0004621923019951a9ab27bdf565ebfc818139
Elle <elli.g.pope@gmail.com>
2017-09-02 17:14:33 -0700

    in menuFunctions() added a className switch for #hamburger called 'clickEvent'

--- script.js
+++ script.js
@@ -24,7 +24,9 @@ var brokenLink = {
 };
 
 //The SVGs will be placed by the javascript. This way, if I ever want to redesign the svg later, I won't have to replace this graphic in every single page and I can keep doing SMIL animations
-var hamMenu = '<svg id="hamburger-menu" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><defs><style>#top, #middle, #bottom {fill: #ffffff;}.transparent{fill: transparent;}</style></defs><rect id="top" y="5" width="50" height="8" rx="4" ry="4"/><animate id="topStart" href="#top" attributeName="y" from="5" to="21" dur=".1s" begin="start.click" fill="freeze"/><animateTransform id="topRotate" href="#top" attributeName="transform" type="rotate" from="0 25 25" to="-45 25 25"	dur=".1s" begin="topStart.end" fill="freeze"/><animateTransform id="topRotateBack" href="#top" attributeName="transform" type="rotate" from="-45 25 25" to="0 25 25" dur=".1s"	begin="end.click"	fill="freeze"/>	<animate id="topEnd" href="#top" attributeName="y" from="21" to="5" dur=".1s" begin="topRotateBack.end" fill="freeze"/><rect id="middle" y="21" width="50" height="8" rx="4" ry="4"/><animate id="middleWidthStart" href="#middle" attributeName="width" from="50" to="2" dur=".01s" begin="start.click + .1s" fill="freeze"/><animate id="middleXStart" href="#middle" attributeName="x" from="0" to="24" dur=".01s" begin="start.click + .1s" fill="freeze"/><animate id="middleWidthEnd" href="#middle" attributeName="width" from="2" to="50" dur=".01s" begin="end.click + .1s" fill="freeze"/><animate id="middleXEnd" href="#middle" attributeName="x" from="24" to="0" dur=".01s" begin="end.click + .1s" fill="freeze"/><rect id="bottom" y="37" width="50" height="8" rx="4" ry="4"/><animate id="bottomStart" href="#bottom" attributeName="y" from="37" to="21" dur=".1s" begin="start.click" fill="freeze"/><animateTransform id="bottomRotate" href="#bottom" attributeName="transform" type="rotate" from="0 25 25" to="45 25 25" dur=".1s" begin="bottomStart.end" fill="freeze"/><animateTransform id="bottomRotateBack" href="#bottom" attributeName="transform" type="rotate" from="45 25 25" to="0 25 25" dur=".1s" begin="end.click" fill="freeze"/><animate id="bottomEnd" href="#bottom" attributeName="y" from="21" to="37" dur=".1s" begin="bottomRotateBack.end" fill="freeze"/><rect id="end" y="0" x="0" width="0" height="0" class="transparent"/><animate id="endButton" href="#end" attributeName="width" from="50" to="0" dur=".01s" begin="end.click" fill="freeze"/><animate href="#end" attributeName="height" from="50" to="0" dur=".01s" begin="end.click" fill="freeze"/><animate href="#start" attributeName="width" from="0" to="50" dur=".01s" begin="endButton.end" fill="freeze"/><animate href="#start" attributeName="height" from="0" to="50" dur=".01s" begin="endButton.end" fill="freeze"/><rect id="start" class="transparent" y="0" x="0" width="50" height="50"/><animate id="startButton" href="#start" attributeName="width" from="50" to="0" dur=".01s" begin="start.click" fill="freeze"/><animate href="#start" attributeName="height" from="50" to="0" dur=".01s" begin="start.click" fill="freeze"/><animate href="#end" attributeName="width" from="0" to="50" dur=".01s" begin="startButton.end" fill="freeze"/><animate href="#end" attributeName="height" from="0" to="50" dur=".01s" begin="startButton.end" fill="freeze"/></svg>'
+var hamMenu = '<svg id="hamburger-menu" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><defs><style>#top, #middle, #bottom {fill: #ffffff;}.transparent{fill: transparent;}</style></defs><rect id="top" y="5" width="50" height="8" rx="4" ry="4"/><animate id="topStart" href="#top" attributeName="y" from="5" to="21" dur=".1s" begin="start.click" fill="freeze"/><animateTransform id="topRotate" href="#top" attributeName="transform" type="rotate" from="0 25 25" to="-45 25 25"	dur=".1s" begin="topStart.end" fill="freeze"/><animateTransform id="topRotateBack" href="#top" attributeName="transform" type="rotate" from="-45 25 25" to="0 25 25" dur=".1s"	begin="end.click"	fill="freeze"/>	<animate id="topEnd" href="#top" attributeName="y" from="21" to="5" dur=".1s" begin="topRotateBack.end" fill="freeze"/><rect id="middle" y="21" width="50" height="8" rx="4" ry="4"/><animate id="middleWidthStart" href="#middle" attributeName="width" from="50" to="2" dur=".01s" begin="start.click + .1s" fill="freeze"/><animate id="middleXStart" href="#middle" attributeName="x" from="0" to="24" dur=".01s" begin="start.click + .1s" fill="freeze"/><animate id="middleWidthEnd" href="#middle" attributeName="width" from="2" to="50" dur=".01s" begin="end.click + .1s" fill="freeze"/><animate id="middleXEnd" href="#middle" attributeName="x" from="24" to="0" dur=".01s" begin="end.click + .1s" fill="freeze"/><rect id="bottom" y="37" width="50" height="8" rx="4" ry="4"/><animate id="bottomStart" href="#bottom" attributeName="y" from="37" to="21" dur=".1s" begin="start.click" fill="freeze"/><animateTransform id="bottomRotate" href="#bottom" attributeName="transform" type="rotate" from="0 25 25" to="45 25 25" dur=".1s" begin="bottomStart.end" fill="freeze"/><animateTransform id="bottomRotateBack" href="#bottom" attributeName="transform" type="rotate" from="45 25 25" to="0 25 25" dur=".1s" begin="end.click" fill="freeze"/><animate id="bottomEnd" href="#bottom" attributeName="y" from="21" to="37" dur=".1s" begin="bottomRotateBack.end" fill="freeze"/><rect id="end" y="0" x="0" width="0" height="0" class="transparent"/><animate id="endButton" href="#end" attributeName="width" from="50" to="0" dur=".01s" begin="end.click" fill="freeze"/><animate href="#end" attributeName="height" from="50" to="0" dur=".01s" begin="end.click" fill="freeze"/><animate href="#start" attributeName="width" from="0" to="50" dur=".01s" begin="endButton.end" fill="freeze"/><animate href="#start" attributeName="height" from="0" to="50" dur=".01s" begin="endButton.end" fill="freeze"/><rect id="start" class="transparent" y="0" x="0" width="50" height="50"/><animate id="startButton" href="#start" attributeName="width" from="50" to="0" dur=".01s" begin="start.click" fill="freeze"/><animate href="#start" attributeName="height" from="50" to="0" dur=".01s" begin="start.click" fill="freeze"/><animate href="#end" attributeName="width" from="0" to="50" dur=".01s" begin="startButton.end" fill="freeze"/><animate href="#end" attributeName="height" from="0" to="50" dur=".01s" begin="startButton.end" fill="freeze"/></svg>';
+
+//var hamMenu = '<svg id="hamburger-menu" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><rect id="top" y="5" width="50" height="8" rx="4" ry="4"/><rect id="middle" y="21" width="50" height="8" rx="4" ry="4"/><rect id="bottom" y="37" width="50" height="8" rx="4" ry="4"/></svg>';
 
 //============================================//
 //These are all the event listeners
@@ -65,15 +67,18 @@ function stickyHeader() {
 function menuFunctions(){
 	var navMenu = document.getElementById('navMenu');
 	var navUl = document.getElementById('navUl');
+	var hamburger = document.getElementById('hamburger');
 	
 	if(clickEvent === false){
 		navMenu.className = 'menuShow';
 		navUl.className = 'ulShow fade-in';
+		hamburger.className = 'clickEvent';
 		
 		clickEvent = true;
 	} else if (clickEvent === true){
 		navMenu.className='';
 		navUl.className = 'fade-out';
+		hamburger.className = '';
 		
 		clickEvent = false;
 	}
@@ -85,10 +90,12 @@ function menuFunctions(){
 function resetClickEvent() {
 	var navMenu = document.getElementById('navMenu');
 	var navUl = document.getElementById('navUl');
+	var hamburger = document.getElementById('hamburger');
 
 	if (window.outerWidth > 571.2){
 		navMenu.className = '';
 		navUl.className = '';
+		hamburger.className = '';
 		
 		clickEvent = false;	
 	}
@@ -185,6 +192,6 @@ function openImageBox(el) {
 //This function loads all svg images into the page
 
 function loadSVG() {
-	var divMenu = document.getElementById('hamburger');
-	hamburger.innerHTML = hamMenu;
+//	var divMenu = document.getElementById('hamburger');
+//	hamburger.innerHTML = hamMenu;
 }
