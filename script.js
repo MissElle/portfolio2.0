31044d240b7cd9c7e839a57b996ddc0b8fa394cb
Elle <elli.g.pope@gmail.com>
2017-09-02 17:17:13 -0700

    removed the function for loading the svgs.... will be using paths to do animations instead

--- script.js
+++ script.js
@@ -23,18 +23,12 @@ var brokenLink = {
 	'description' : '<h2>Uh Oh!<br> A Broken Link!</h2> <br><p>It looks like the artist programmed this site wrong! Silly Artist, thinking she&#39;s a programmer...</p>'
 };
 
-//The SVGs will be placed by the javascript. This way, if I ever want to redesign the svg later, I won't have to replace this graphic in every single page and I can keep doing SMIL animations
-var hamMenu = '<svg id="hamburger-menu" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><defs><style>#top, #middle, #bottom {fill: #ffffff;}.transparent{fill: transparent;}</style></defs><rect id="top" y="5" width="50" height="8" rx="4" ry="4"/><animate id="topStart" href="#top" attributeName="y" from="5" to="21" dur=".1s" begin="start.click" fill="freeze"/><animateTransform id="topRotate" href="#top" attributeName="transform" type="rotate" from="0 25 25" to="-45 25 25"	dur=".1s" begin="topStart.end" fill="freeze"/><animateTransform id="topRotateBack" href="#top" attributeName="transform" type="rotate" from="-45 25 25" to="0 25 25" dur=".1s"	begin="end.click"	fill="freeze"/>	<animate id="topEnd" href="#top" attributeName="y" from="21" to="5" dur=".1s" begin="topRotateBack.end" fill="freeze"/><rect id="middle" y="21" width="50" height="8" rx="4" ry="4"/><animate id="middleWidthStart" href="#middle" attributeName="width" from="50" to="2" dur=".01s" begin="start.click + .1s" fill="freeze"/><animate id="middleXStart" href="#middle" attributeName="x" from="0" to="24" dur=".01s" begin="start.click + .1s" fill="freeze"/><animate id="middleWidthEnd" href="#middle" attributeName="width" from="2" to="50" dur=".01s" begin="end.click + .1s" fill="freeze"/><animate id="middleXEnd" href="#middle" attributeName="x" from="24" to="0" dur=".01s" begin="end.click + .1s" fill="freeze"/><rect id="bottom" y="37" width="50" height="8" rx="4" ry="4"/><animate id="bottomStart" href="#bottom" attributeName="y" from="37" to="21" dur=".1s" begin="start.click" fill="freeze"/><animateTransform id="bottomRotate" href="#bottom" attributeName="transform" type="rotate" from="0 25 25" to="45 25 25" dur=".1s" begin="bottomStart.end" fill="freeze"/><animateTransform id="bottomRotateBack" href="#bottom" attributeName="transform" type="rotate" from="45 25 25" to="0 25 25" dur=".1s" begin="end.click" fill="freeze"/><animate id="bottomEnd" href="#bottom" attributeName="y" from="21" to="37" dur=".1s" begin="bottomRotateBack.end" fill="freeze"/><rect id="end" y="0" x="0" width="0" height="0" class="transparent"/><animate id="endButton" href="#end" attributeName="width" from="50" to="0" dur=".01s" begin="end.click" fill="freeze"/><animate href="#end" attributeName="height" from="50" to="0" dur=".01s" begin="end.click" fill="freeze"/><animate href="#start" attributeName="width" from="0" to="50" dur=".01s" begin="endButton.end" fill="freeze"/><animate href="#start" attributeName="height" from="0" to="50" dur=".01s" begin="endButton.end" fill="freeze"/><rect id="start" class="transparent" y="0" x="0" width="50" height="50"/><animate id="startButton" href="#start" attributeName="width" from="50" to="0" dur=".01s" begin="start.click" fill="freeze"/><animate href="#start" attributeName="height" from="50" to="0" dur=".01s" begin="start.click" fill="freeze"/><animate href="#end" attributeName="width" from="0" to="50" dur=".01s" begin="startButton.end" fill="freeze"/><animate href="#end" attributeName="height" from="0" to="50" dur=".01s" begin="startButton.end" fill="freeze"/></svg>';
-
-//var hamMenu = '<svg id="hamburger-menu" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><rect id="top" y="5" width="50" height="8" rx="4" ry="4"/><rect id="middle" y="21" width="50" height="8" rx="4" ry="4"/><rect id="bottom" y="37" width="50" height="8" rx="4" ry="4"/></svg>';
-
 //============================================//
 //These are all the event listeners
 
 window.addEventListener('scroll', stickyHeader);
 document.getElementById('hamburger').addEventListener('click', menuFunctions);
 window.addEventListener('load', isMobile);
-window.addEventListener('load', loadSVG);
 window.addEventListener('resize', detectAspectRatio);
 window.addEventListener('resize', resetClickEvent);
 document.getElementById('cross').addEventListener('click', closeImageBox);
@@ -186,12 +180,4 @@ function openImageBox(el) {
 	detectAspectRatio();
 	imgBox.style.display = 'flex';
 	imgBox.className = 'fade-in';
-}
-
-//============================================//
-//This function loads all svg images into the page
-
-function loadSVG() {
-//	var divMenu = document.getElementById('hamburger');
-//	hamburger.innerHTML = hamMenu;
 }
