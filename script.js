9303f9b5ad9a6383d8da72a40efbb8e64107ef62
Elle <elli.g.pope@gmail.com>
2017-09-12 19:21:24 -0700

    writing a function to sort the array by date (also added date fields to the images array... not real dates, but in date format). Not completed but computer is crashing so need to commit this.

--- script.js
+++ script.js
@@ -17,43 +17,78 @@ var images = [
 	'source' : 'images/illustration-colors-peter-buxton.jpg',
 	'sourceSmall' : 'images/illustration-colors-peter-buxton-400.jpg',
 	'preview' : 'images/illustration-colors-peter-buxton-preview.jpg',
+	'date' : 20161203,
 	'description' : '<h1>Peter Buxton</h1><h3>Colors: The Tuskegee Studies</h3><p>December 2016</p><p>A video game thit is also a piece of historical fiction, Colors: The Tuskegee Studies takes a "clear" look into a darker side of America&#39;s history. Based upon the true Peter Buxton who helped take down one of the most racist "studies" conducted under the name of medicine.</p>'},
 	{'name' : 'illustration-colors-claire-newman',
 	'category' : 'Colors: The Tuskegee Studies',
 	'source' : 'images/illustration-colors-claire-newman.jpg',
 	'sourceSmall' : 'images/illustration-colors-claire-newman-400.jpg',
 	'preview' : 'images/illustration-colors-claire-newman-preview.jpg',
+	'date' : 20161204,
 	'description' : '<h1>Claire Newman</h1><h3>Colors: The Tuskegee Studies</h3><p>December 2016</p><p>A video game thit is also a piece of historical fiction, Colors: The Tuskegee Studies takes a "clear" look into a darker side of America&#39;s history. Main character Claire Newman is a slacktivist who went against her father&#39;s and moved to Tuskegee, Alabama with nothing but a nursing degree and free time. However, something is going on in Tuskegee that&#39;s not quite right. Interviewing the people in the area, the truth unravels.</p>'},
 	{'name' : 'illustration-colors-clementine-turner',
 	'category' : 'Colors: The Tuskegee Studies',
 	'source' : 'images/illustration-colors-clementine-turner.jpg',
 	'sourceSmall' : 'images/illustration-colors-clementine-turner-400.jpg',
 	'preview' : 'images/illustration-colors-clementine-turner-preview.jpg',
+	'date' : 20161205,
 	'description' : '<h1>Clementine Turner</h1><h3>Colors: The Tuskegee Studies</h3><p>December 2016</p><p>A video game thit is also a piece of historical fiction, Colors: The Tuskegee Studies takes a "clear" look into a darker side of America&#39;s history. Supporting character Clementine Turner is Claire&#39;s aunt and only point of contact in Tuskegee. Holding a grudge against her brother, Claire&#39;s father, she lets Claire move from San Francisco to live with her in Tuskegee. Behind her bitterness, there is history or pain, racism, abandonement, and an understanding of how people behave.</p>'},
 	{'name' : 'illustration-colors-eunice-rivers',
 	'category' : 'Colors: The Tuskegee Studies',
 	'source' : 'images/illustration-colors-eunice-rivers.jpg',
 	'sourceSmall' : 'images/illustration-colors-eunice-rivers-400.jpg',
 	'preview' : 'images/illustration-colors-eunice-rivers-preview.jpg',
+	'date' : 20161206,
 	'description' : '<h1>Eunice Rivers</h1><h3>Colors: The Tuskegee Studies</h3><p>December 2016</p><p>A video game thit is also a piece of historical fiction, Colors: The Tuskegee Studies takes a "clear" look into a darker side of America&#39;s history. Eunice Rivers is based on real person Eunice Rivers, who helped to conduct the Tuskegee Studies for nearly 50 years. This game will explore her character in ways other media have not yet explored, hopefully to answer the question, how can someone not see blatant racism in front of them?</p>'},
 	{'name' : 'illustration-colors-al',
 	'category' : 'Colors: The Tuskegee Studies',
 	'source' : 'images/illustration-colors-al.jpg',
 	'sourceSmall' : 'images/illustration-colors-al-400.jpg',
 	'preview' : 'images/illustration-colors-al-preview.jpg',
+	 'date' : 20161207,
 	'description' : '<h1>Subject Al</h1><h3>Colors: The Tuskegee Studies</h3><p>December 2016</p><p>A video game thit is also a piece of historical fiction, Colors: The Tuskegee Studies takes a "clear" look into a darker side of America&#39;s history. Fictional character Al is an untreated syphilis patient who is discovered by Claire while she is on drugs. While Claire tries to get him treatment, the hosptial refuses.</p>'},
 	{'name' : 'illustration-mi-bhickie',
 	'category' : 'My oh M.I.',
 	'source' : 'images/illustration-mi-bhickie.jpg',
 	'sourceSmall' : 'images/illustration-mi-bhickie-400.jpg',
 	'preview' : 'images/illustration-mi-bhickie-preview.jpg',
+	 'date' : 20170201,
 	'description' : '<h1>Bhictoria Darlene Beattie</h1><h3>My oh M.I.</h3><p>February 2017</p><p>This comic series is a look into mental illness, its effects on society and its effects on a person. Bhictoria, or Bhickie for short, is a mid-20&#39;s recent college graduate who cannot seem to relate to others. As the story progesses she finds herslef frustrated with the expectations of her society, but more frustrated with herself. Can she be happy and still a good person?</p>'},
 	{'name' : 'illustration-mi-sage',
 	'category' : 'My oh M.I.',
 	'source' : 'images/illustration-mi-sage.jpg',
 	'sourceSmall' : 'images/illustration-mi-sage-400.jpg',
 	'preview' : 'images/illustration-mi-sage-preview.jpg',
-	'description' : '<h1>Sage Markus Easom</h1><h3>My oh M.I.</h3><p>February 2017</p><p>This comic series is a look into mental illness, its effects on society and its effects on a person. Sage, or his nickname Markus, is a mid-20&#39;s college dropout who relates well with others but often feels that when people interact with one another, it&#39;s usually a facade. As the story progesses he finds himself fascinated with Bhickie and her contempt towards the world. But are his feelings for her healthy?</p>'}
+	 'date' : 20170202,
+	'description' : '<h1>Sage Markus Easom</h1><h3>My oh M.I.</h3><p>February 2017</p><p>This comic series is a look into mental illness, its effects on society and its effects on a person. Sage, or his nickname Markus, is a mid-20&#39;s college dropout who relates well with others but often feels that when people interact with one another, it&#39;s usually a facade. As the story progesses he finds himself fascinated with Bhickie and her contempt towards the world. But are his feelings for her healthy?</p>'},
+	{'name' : 'illustration-doodle-crow-three',
+	'category' : 'Doodles',
+	'source' : 'images/illustration-doodle-crow-three.jpg',
+	'sourceSmall' : 'images/illustration-doodle-crow-three-400.jpg',
+	'preview' : 'images/illustration-doodle-crow-three-preview.jpg',
+	 'date' : 20170702,
+	'description' : '<h1>Crow Study Three</h1><h3>Doodles</h3><p>July 2017</p><p>A quick study based on a photograph that was taken.  Ink Brush.</p>'},
+	{'name' : 'illustration-doodle-crow-one',
+	'category' : 'Doodles',
+	'source' : 'images/illustration-doodle-crow-one.jpg',
+	'sourceSmall' : 'images/illustration-doodle-crow-one-400.jpg',
+	'preview' : 'images/illustration-doodle-crow-one-preview.jpg',
+	 'date' : 20170601,
+	'description' : '<h1>Crow Study Two</h1><h3>Doodles</h3><p>July 2017</p><p>A quick study that was done with ink with varying sensitivity. Playing with negative vs subject. Reversed Imagery with Mobile Studio. </p>'},
+	{'name' : 'illustration-doodle-crow-two',
+	'category' : 'Doodles',
+	'source' : 'images/illustration-doodle-crow-two.jpg',
+	'sourceSmall' : 'images/illustration-doodle-crow-two-400.jpg',
+	'preview' : 'images/illustration-doodle-crow-two-preview.jpg',
+	 'date' : 20170602,
+	'description' : '<h1>Crow Study One</h1><h3>Doodles</h3><p>June 2017</p><p>A quick study that was done with ink with varying sensitivity. Playing with negative vs subject.</p>'},
+	{'name' : 'illustration-doodle-bad-dreams',
+	'category' : 'Doodles',
+	'source' : 'images/illustration-doodle-bad-dreams.jpg',
+	'sourceSmall' : 'images/illustration-doodle-bad-dreams-400.jpg',
+	'preview' : 'images/illustration-doodle-bad-dreams-preview.jpg',
+	'date' : 20170501,
+	'description' : '<h1>Bad Dreams</h1><h3>Doodles</h3><p>May 2017</p><p>Study playing with wet vs dry brushing. Representation of Lucidity.</p>'}
 ];
 
 var brokenLink = {
@@ -182,7 +217,7 @@ function detectAspectRatio() {
 		descriptionSide.style.width = '30%';
 		descriptionSide.style.height = '100%';
 		changeoutText.style.textAlign = 'right';
-		for(var i=0; i<para.lenth; ++i){
+		for(var i=0; i<para.length; ++i){
 			para[i].style.textAlign = 'right';
 		}
 	}else {
@@ -191,7 +226,7 @@ function detectAspectRatio() {
 		descriptionSide.style.width ='100%';
 		descriptionSide.style.height = '30%';
 		changeoutText.style.textAlign = 'left';
-		for(var i=0; i<para.lenth; ++i){
+		for(var i=0; i<para.length; ++i){
 			para[i].style.textAlign = 'left';
 		}
 	}
@@ -248,6 +283,19 @@ function loadEmoticons() {
 }
 
 //============================================//
+//This function will sort the array by date
+
+function sortNumber(a, b) {
+	return a-b;
+}
+
+function sortArray() {
+	for (var date in images){
+		images.sort(sortNumber);
+	}
+}
+
+//============================================//
 //This function detects which page the user is on
 
 function detectPage() {
@@ -265,6 +313,12 @@ function detectPage() {
 		pageH1.appendChild(pageH1text);
 		mainPage.appendChild(pageH1);
 		
+		newH2 = document.createElement('h2');
+		newH2Text = document.createTextNode('Sketchbook');
+		newH2.appendChild(newH2Text);
+		mainPage.appendChild(newH2);
+		generateImgPreviews('Doodles', images);
+		
 		var newH2 = document.createElement('h2');
 		var newH2Text = document.createTextNode('Concept Art');
 		newH2.appendChild(newH2Text);
@@ -277,11 +331,6 @@ function detectPage() {
 		mainPage.appendChild(newH2);
 		generateImgPreviews('My oh M.I.', images);
 		
-		newH2 = document.createElement('h2');
-		newH2Text = document.createTextNode('Sketchbook');
-		newH2.appendChild(newH2Text);
-		mainPage.appendChild(newH2);
-		generateImgPreviews('Doodles', images);
 	}else if (webdev !== null){
 		console.log('you are in the Web Development page!');
 	}else{
